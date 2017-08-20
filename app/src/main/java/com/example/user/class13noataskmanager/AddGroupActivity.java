d94d0537208414303d354abd51317909d3238a5b
noarot <noa_rot@yahoo.com>
2017-08-20 10:24:45 +0300

    1a

--- app/src/main/java/com/example/user/class13noataskmanager/AddGroupActivity.java
+++ app/src/main/java/com/example/user/class13noataskmanager/AddGroupActivity.java
@@ -3,7 +3,7 @@ package com.example.user.class13noataskmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class AddGroupActivity extends AppCompatActivity {
+public class  AddGroupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
